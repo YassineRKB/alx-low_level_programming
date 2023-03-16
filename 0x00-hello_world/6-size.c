eb44f3c0ade07efd5c0f616a8bf56478573a625d
Yassine Rakibi <yassine@arcraven.com>
2023-03-16 19:04:49 +0100

    6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
 */
 int main(void)
 {
-	printf("Size of a char:%s", sizeof(char));
-	printf("Size of an int:%s", sizeof(int));
-	printf("Size of a long:%s", sizeof(long));
-	printf("Size of a long long:%s", sizeof(long long));
-	printf("Size of a float:%s", sizeof(float));
+	printf("Size of a char: %s byte(s)", sizeof(char));
+	printf("Size of an int: %ld byte(s)", sizeof(int));
+	printf("Size of a long: %ld byte(s)", sizeof(long));
+	printf("Size of a long long int: %ld byte(s)", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)", sizeof(float));
 	return (0);
 }
