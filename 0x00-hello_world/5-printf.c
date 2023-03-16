e308e7f81d7ced904cc55440dd2ba66eb91eff48
Yassine Rakibi <yassine@arcraven.com>
2023-03-16 18:28:58 +0100

    5-prints

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+*	File: 5-printf.c
+*	Desc: program that prints qoute using printf func
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
