94ba973e80a76c32dfdc9aa5720b919564d7263e
Yassine Rakibi <yassine@arcraven.com>
2023-03-16 18:23:49 +0100

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
 *	Name: 4-puts.c
 *	Desc: hello world program using puts function.
 */
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
