c913b36fd33793b39400c4f79c48d6a3e69d0980
Yassine Rakibi <yassine@arcraven.com>
2023-03-17 18:21:39 +0100

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,25 @@
-#include <std.io>
+#include <stdio.h>
 /**
 *	main - Entry Point
-*	Description: assign random number to var n
+*	Description: prints all possible combinations for single digit
 *	Return: 0
 */
+int main(void)
+{
+	int i;
+	int d = 48;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(d);
+		d++;
+		if (i == 9)
+		{
+			continue;
+		}
+		putchar(',');
+	}
+	putchar('\n');
+	return (0);
+}
 
