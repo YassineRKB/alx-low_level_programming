09d19122256f3519b2fb72348810b2201d7b42d7
Yassine Rakibi <yassine@arcraven.com>
2023-03-17 16:58:58 +0100

    2-print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,18 @@
-#include <std.io>
+#include <stdio.h>
 /**
 *	main - Entry Point
-*	Description: assign random number to var n
+*	Description: prints the alphabets
 *	Return: 0
 */
+int main(void)
+{
+	int i;
+	char a[26] = "abcdefghijklmnopqrstuvwxyz";
 
+	for (i = 0; i < 26; i++)
+	{
+		putchar(a[i]);
+	}
+	putchar('\n');
+	return (0);
+}
