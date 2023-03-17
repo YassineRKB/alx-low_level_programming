0e6a25fb5a76ab8d986b30be9e54514c62bcff21
Yassine Rakibi <yassine@arcraven.com>
2023-03-17 17:04:04 +0100

    3-print_alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,23 @@
-#include <std.io>
+#include <stdio.h>
 /**
 *	main - Entry Point
-*	Description: assign random number to var n
+*	Description: prints the alphabets in lowercase then uppercase
 *	Return: 0
 */
+int main(void)
+{
+	int i;
+	char a[26] = "abcdefghijklmnopqrstuvwxyz";
+	char b[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+	for (i = 0; i < 26; i++)
+	{
+		putchar(a[i]);
+	}
+	for (i = 0; i < 26; i++)
+	{
+		putchar(b[i]);
+	}
+	putchar('\n');
+	return (0);
+}
