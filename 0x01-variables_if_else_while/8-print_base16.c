166fd0bee44185c0fce0c41c9dc5497af6832e5f
Yassine Rakibi <yassine@arcraven.com>
2023-03-17 17:56:30 +0100

    8-print_base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,7 +1,18 @@
-#include <std.io>
+#include <stdio.h>
 /**
 *	main - Entry Point
-*	Description: assign random number to var n
+*	Description: prints the base 16 numbers
 *	Return: 0
 */
+int main(void)
+{
+	int i;
+	char a[16] = "0123456789abcdef";
 
+	for (i = 0; i < 16; i++)
+	{
+		putchar(a[i]);
+	}
+	putchar('\n');
+	return (0);
+}
