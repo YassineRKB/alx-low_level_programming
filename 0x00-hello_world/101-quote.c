004f43b325215d45ef5db80b44e3d4f2e0728050
Yassine Rakibi <yassine@arcraven.com>
2023-03-17 15:38:53 +0100

    0x01-variables_if_else_while

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+/**
+*	main - Entry Point
+*	Description: prints qoute and returns 1
+*	Return: 1
+*/
+int main(void)
+{
+	string qoute[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	std::cout << "titt" <<
+	return (1);
+}
