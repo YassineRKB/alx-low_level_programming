67e9e3d7cd4d2f171d3ff274b1d7c68814436652
Yassine Rakibi <yassine@arcraven.com>
2023-03-17 17:15:47 +0100

    5-print_numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,11 @@
 #include <std.io>
 /**
 *	main - Entry Point
-*	Description: assign random number to var n
+*	Description: prints base 10 numbers
 *	Return: 0
 */
-
+int main(void)
+{
+	printf("0123456789");
+	return (0);
+}
