8a1ed4b5ef2eb22f9d891b595793952598cb8418
Yassine Rakibi <yassine@arcraven.com>
2023-03-21 21:47:29 +0100

    5-print_sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_sign - entry point to the check
+* Descrition: checks the sign of the int n
+* @n: int to be checked
+* Return: 0 or 1 or -1
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	return (+1);
+
+	if (n == 0)
+	return (0);
+
+	if (n < 0)
+	return (-1);
+}
