5e1ed1e562ab44f9d41b2a13ed8306d697a0fc4d
Yassine Rakibi <yassine@arcraven.com>
2023-03-22 00:51:04 +0100

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,24 +13,35 @@ void print_times_table(int n)
 
 		for (i = 0; i <= n; i++)
 		{
-			_putchar('0');
-			for (j = 1; j <= n; j++)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar(',');
-				_putchar(' ');
 				r = i * j;
-				if (r <= 9)
+				if (r < 10)
 				{
-					_putchar(' ');
-					_putchar(' ');
+					if (j != 10)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+					}
 					_putchar(r + '0');
 				}
-				else
+				else if (r < 100)
 				{
+					_putchar(' ');
 					_putchar(' ');
 					_putchar((r / 10) + '0');
 					_putchar((r % 10) + '0');
 				}
+				else
+				{
+					_putchar(' ');
+					_putchar((r / 100) + '0');
+					_putchar(((r % 100) / 10) + '0');
+					_putchar((r % 10) + '0');
+				}
+				if (j != n)
+					_putchar(',');
 			}
 			_putchar('\n');
 		}
