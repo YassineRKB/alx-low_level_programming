a14e877e209ead49c1ce5c1350027ef0266c5e3e
Yassine Rakibi <yassine@arcraven.com>
2023-03-21 22:24:56 +0100

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,7 +3,7 @@
 * print_last_digit - entry point to the code
 * Description: prints the last digit of a number n
 * @n: int number to be checked
-* Return: 0
+* Return: last digit
 **/
 int print_last_digit(int n)
 {
@@ -14,5 +14,5 @@ int print_last_digit(int n)
 		last *= -1;
 	}
 	_putchar(last + '0');
-	return (0);
+	return (last);
 }
