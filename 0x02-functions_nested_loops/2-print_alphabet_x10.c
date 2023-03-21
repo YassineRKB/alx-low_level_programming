b7de7567af8148cd880ad8992bbe468fb8363e30
Yassine Rakibi <yassine@arcraven.com>
2023-03-21 21:10:06 +0100

    2-alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+// print_alphabet_x10 - entrypoint.
+// descriptions: prints the alphabet 10 times
+void print_alphabet_x10(void)
+{
+	char ar[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i,l;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (l = 0; l < 26;l++)
+		{
+			_putchar(ar[j]);
+		}
+	_putchar('\n');
+	}
+}
