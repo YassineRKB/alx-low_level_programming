7d7ac8a40610a0665c1ea0860d92a563ec7383c3
Yassine Rakibi <yassine@arcraven.com>
2023-03-21 23:44:58 +0100

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/*
+* add(int,int) - entry point to code
+* description: add numbers a and b
+* Return: the result of a+b
+**/
+int add(int a, int b)
+{
+	int r = a + b;
+
+	return (r);
+}
