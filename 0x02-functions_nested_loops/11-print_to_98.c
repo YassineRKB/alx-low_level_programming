238a74574d00c8a76c152898ec544e343a128bb6
Yassine Rakibi <yassine@arcraven.com>
2023-03-22 00:17:19 +0100

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,9 +8,10 @@
 **/
 void print_to_98(int n)
 {
+
 	if (n <= 98)
 	{
-		for (n; n > 98; n++)
+		for (; n < 98; n++)
 		{
 			printf("%d, ", n);
 		}
@@ -18,7 +19,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (n; n > 98; n--)
+		for (; n > 98; n--)
 		{
 			printf("%d, ", n);
 		}
