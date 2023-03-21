e206d0cba1a0f7c0411632283d9faa98aa155d3a
Yassine Rakibi <yassine@arcraven.com>
2023-03-21 23:20:57 +0100

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* name times_table - Entry Point to the code
+* description - prints the 9 time tables
+* Return: 9 time tables
+*/
+void times_table(void)
+{
+	int i, j, r;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			r = i * j;
+			if ()
+			{
+				_putchar(r + '0');
+			}
+			else
+			{
+				_putchar((r / 10) + '0');
+				_putchar((r % 10) + '0');
+			}
+		}
+	}
+}
