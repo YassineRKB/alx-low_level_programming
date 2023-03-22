244b37e88bcdfff9c4ec5bd5f848428ac8b1f85b
Yassine Rakibi <yassine@arcraven.com>
2023-03-22 01:33:31 +0100

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+/**
+*main - entry point
+*description: prints the first 50 Fibonacci numbers
+*Return: 0
+**/
+int main(void)
+{
+	long i, j, f;
+	int n = 0;
+
+	i = 0;
+	j = 1;
+
+	while (n < 50)
+	{
+		f = i + j;
+		if (n == 49)
+		{
+			print("%ld\n", f);
+		}
+		else
+		{
+			print("%ld, ", f);
+		}
+		i = j;
+		j = f;
+		n++;
+	}
+	return (0);
+}
