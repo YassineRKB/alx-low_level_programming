6f347c4b32bfecdfcd7d34d9af5dea2adf9a9b8f
Yassine Rakibi <yassine@arcraven.com>
2023-03-22 01:46:30 +0100

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+*main - entry point
+*descrition: prints even values of fib seq
+*Return: 0
+**/
+int main(void)
+{
+	long j, r, s;
+	int i = 0;
+
+	while (r + j < 4000000)
+	{
+		r += j;
+		if (r % 2 == 0)
+		{
+			s += r;
+			j = r - j;
+			++i;
+		}
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
