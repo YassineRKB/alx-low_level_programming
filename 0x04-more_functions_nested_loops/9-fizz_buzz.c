63badee5e53d9b492119487627781315d87b49fe
Yassine Rakibi <yassine@arcraven.com>
2023-03-23 21:33:24 +0100

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+* main - entry point to “Fizz-Buzz test” code.
+* Return: 0.
+*/
+int main(void)
+{
+    int i;
+
+    for (i = 1; i <= 100; i++)
+    {
+        if (i % 3 == 0 && i % 5 != 0)
+        {
+            printf("Fizz");
+        }
+        else if (i % 3 != 0 && i % 5 == 0)
+        {
+            printf("Buzz");
+        }
+        else if (i % 3 == 0 && i % 5 == 0)
+        {
+            printf("FizzBuzz");
+        }
+        else
+        {
+            printf("%d", i);
+        }
+    }
+    putchar('\n');
+}
