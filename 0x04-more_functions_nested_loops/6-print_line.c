188561d2fa0aedea3d7496792265caa7e7af3710
Yassine Rakibi <yassine@arcraven.com>
2023-03-23 12:51:46 +0100

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_line - function that draws stright line
+* @n: int number of times _ should be drawn
+* Return: nothing.
+*/
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
