758e22e1a1e1247ddea04edae749ed85e49c2e7e
Yassine Rakibi <yassine@arcraven.com>
2023-03-23 21:36:14 +0100

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,26 +5,26 @@
 */
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 1; i <= 100; i++)
-    {
-        if (i % 3 == 0 && i % 5 != 0)
-        {
-            printf("Fizz");
-        }
-        else if (i % 3 != 0 && i % 5 == 0)
-        {
-            printf("Buzz");
-        }
-        else if (i % 3 == 0 && i % 5 == 0)
-        {
-            printf("FizzBuzz");
-        }
-        else
-        {
-            printf("%d", i);
-        }
-    }
-    putchar('\n');
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 3 != 0 && i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+	}
+	putchar('\n');
 }
