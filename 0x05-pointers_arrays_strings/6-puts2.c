45d97cc5f37f424d86fad6f009a7f822a66f734d
Yassine Rakibi <yassine@arcraven.com>
2023-03-27 23:25:29 +0200

    task 6 works in compiler /[{},{}\]

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,13 +8,19 @@
 void puts2(char *str)
 {
 	int i = 0;
+	int k = 0;
 
 	while (str[i] != '\0')
 	{
-		if (i % 2 == 0)
+		if (k == 0)
 		{
 			_putchar(str[i]);
 			i++;
+			k++;
+		}
+		else if (k == 1)
+		{
+			k--;
 		}
 	}
 	_putchar('\n');
