f80ff4fce91bc5e82ac9ab798c1cfc3feb55117b
Yassine Rakibi <yassine@arcraven.com>
2023-03-27 12:12:27 +0200

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *  reset_to_98 - function that takes pointer to an int
+ *  @n: pointer pointing to type int
+ *  Return: n
+ ***/
+
+void reset_to_98(int *n)
+{
+	n = 98;
+	return (98);
+}
