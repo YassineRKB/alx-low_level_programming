3576f493a9c4024cf4a7aa972c9c45f610354d34
Yassine Rakibi <yassine@arcraven.com>
2023-03-27 13:57:10 +0200

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+# include "main.h"
+/**
+* print_rev - function that prints string
+* @s: pointer pointing to char
+* Return: nothing.
+*/
+
+void print_rev(char *s)
+{
+	int len = 0;
+	int i;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	i -= 1;
+	while (i >= 0)
+	{
+		_putchar(*(s + i));
+		i--;
+	}
+	_putchar('\n');
+}
