732fa7cb99235631664c256e309d39257ba5cedd
Yassine Rakibi <yassine@arcraven.com>
2023-03-30 12:08:39 +0200

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,11 @@
+# include "main.h"
+/**
+* _strcat - function
+* @src: source string
+* @dest: destination string
+* Return: pointer
+**/
+char *_strcat(char *dest, char *src)
+{
+	return (dest);
+}
