a0ffc6917578e8fa8b86e1a0bb3a0945683506d1
Yassine Rakibi <yassine@arcraven.com>
2023-03-31 00:32:51 +0200

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+# include "main.h"
+
+/**
+* cap_string - function that capitalizes all words of a string.
+* @a: array
+* Return: char
+**/
+
+char *cap_string(char *a)
+{
+	int len = 0;
+	char sep[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+	while (a[len] != '\0')
+	{
+		if ((a[len] >= 97 && a[len] <= 122))
+		{
+			if (len == 0)
+			{
+				a[len] = a[len] - 32;
+			}
+			else
+			{
+				for (int j = 0; j <= 12; j++)
+				{
+					if (sep[j] == a[len - 1])
+					{
+						a[len] = a[len] - 32;
+					}
+				}
+			}
+		}
+		len++;
+	}
+	return (a);
+}
