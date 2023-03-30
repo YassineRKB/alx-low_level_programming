c1930762bbb6b245810d6c9f1fce4608e9297125
Yassine Rakibi <yassine@arcraven.com>
2023-03-31 00:36:49 +0200

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,29 +8,26 @@
 
 char *cap_string(char *a)
 {
-	int len = 0;
-	char sep[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int i, j;
+	int sep[] = {',', ';', '.', '?', '"',
+	'(', ')', '{', '}', ' ', '\n', '\t'};
 
-	while (a[len] != '\0')
+	for (i = 0; a[i] != '\0'; i++)
 	{
-		if ((a[len] >= 97 && a[len] <= 122))
+		if (a[i] >= 'a' && n[i] <= 'z')
 		{
-			if (len == 0)
+			n[i] = n[i] - 32;
+		}
+
+		cap = 0;
+
+		for (j = 0; j <= 12; j++)
+		{
+			if (n[i] == sep[j])
 			{
-				a[len] = a[len] - 32;
-			}
-			else
-			{
-				for (int j = 0; j <= 12; j++)
-				{
-					if (sep[j] == a[len - 1])
-					{
-						a[len] = a[len] - 32;
-					}
-				}
+				x = 12;
 			}
 		}
-		len++;
 	}
 	return (a);
 }
