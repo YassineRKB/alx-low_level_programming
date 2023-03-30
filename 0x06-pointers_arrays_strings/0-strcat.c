1aaf28523b2fb78abddefadf33270dfba9fbfb39
Yassine Rakibi <yassine@arcraven.com>
2023-03-30 12:10:33 +0200

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,5 +7,21 @@
 **/
 char *_strcat(char *dest, char *src)
 {
+	int len = 0;
+	int i;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[len] = src[i];
+		len++;
+	}
+
+
 	return (dest);
 }
+
