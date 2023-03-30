0f0d4e26c391a54fd8b18d771e64656748924d77
Yassine Rakibi <yassine@arcraven.com>
2023-03-31 00:20:07 +0200

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,20 +9,26 @@
 char *cap_string(char *a);
 {
 	int len = 0;
+	char sep = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	while (a[len])
+	while (a[len] != '\0')
 	{
-		while (!(a[0] >= 97 && a[0] <= 122))
+		if ((a[len] >= 97 && a[len] <= 122))
 		{
-			len++;
-		}
-		if (a[len - 1] == '(' || a[len - 1] == '\t' || a[len - 1] == '\n'
-		|| a[len - 1] == ',' || a[len - 1] == ';' || a[len - 1] == '!'
-		|| a[len - 1] == '.' || a[len - 1] == '?' || a[len - 1] == '.'
-		|| a[len - 1] == '"' || a[len - 1] == ' ' || a[len - 1] == '}'
-		|| a[len - 1] == '{' || a[len - 1] == ')' || len == 0)
-		{
-			a[len] = a[len] - 32;
+			if (len == 0)
+			{
+				a[len] = a[len] - 32;
+			}
+			else
+			{
+				for (int j = 0; j <= 12; j++)
+				{
+					if (sep[j] == a[len - 1])
+					{
+						a[len] = a[len] - 32;
+					}
+				}
+			}
 		}
 		len++;
 	}
