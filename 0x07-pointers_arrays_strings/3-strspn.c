1945ee73cd216c53dec80750d43c7b5d2aa1cb36
Yassine Rakibi <yassine@arcraven.com>
2023-04-03 18:08:25 +0200

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,7 +8,7 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int len;
+	unsigned int len = 0;
 	int i, j;
 
 	for (i = 0; s[i] != 0; i++)
@@ -20,7 +20,7 @@ unsigned int _strspn(char *s, char *accept)
 				len++;
 				break;
 			}
-			else if (accept[j+1] == '\0')
+			else if (accept[j + 1] == '\0')
 			{
 				return (len);
 			}
