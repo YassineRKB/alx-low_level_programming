9f9e724ce3294b723fb700391cb69f81ab6e002a
Yassine Rakibi <yassine@arcraven.com>
2023-04-03 19:13:22 +0200

    task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,14 +10,18 @@ char *_strpbrk(char *s, char *accept)
 	unsigned int i;
 	unsigned int j;
 
-	for (j = 0; s[j]; j++)
+	for (j = 0; *(s + j) != '\0'; j++)
 	{
-		for (i = 0; accept[i] != '\0'; i++)
+		for (i = 0; *(accept + i) != '\0'; i++)
 		{
-			if (s[i] == accept[i])
+			if (*(accept + i) == *(s + j))
 			{
-				return ((s + i - 1));
+				return (s + i);
 			}
+		}
+		if (*(accept + i) == *(s + j))
+		{
+			return (s + i);
 		}
 	}
 	return ('\0');
