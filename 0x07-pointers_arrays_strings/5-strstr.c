26a08174a8dbb7f6ba8a0d01f2348583499c53c0
Yassine Rakibi <yassine@arcraven.com>
2023-04-03 23:19:42 +0200

    task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+# include "main.h"
+/**
+* _strstr - function that locates a substring.
+* @haystack: string to scan
+* @needle: string to match
+* Return: Pointer or Null.
+**/
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	if (*needle == 0)
+	{
+		return (haystack);
+	}
+
+	while (*haystack)
+	{
+		for (i = 0; haystack[i] == needle[i]; i++)
+		{
+			if (needle[i] != '\0')
+			{
+				return (haystack);
+			}
+		}
+		haystack++;
+	}
+	return ('\0');
+}
