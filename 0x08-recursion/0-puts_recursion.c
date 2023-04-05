57f73755474a20ad2fb8501fb69485bc5321859e
Yassine Rakibi <yassine@arcraven.com>
2023-04-05 08:45:28 +0200

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,11 +6,14 @@
 **/
 void _puts_recursion(char *s)
 {
-	int len = 0;
-
-	while (s[len] != 0)
+	if (*s == '\0')
 	{
-		_putchar(s[len] + '0');
-		len++;
+		_putchar('\n');
+	}
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
 	}
 }
