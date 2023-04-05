30332bd8fb220c5cfa8bd60dfd96b89e7b643977
Yassine Rakibi <yassine@arcraven.com>
2023-04-05 08:27:26 +0200

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,1 +1,16 @@
 # include "main.h"
+/**
+* _puts_recursion - function that prints a string, followed by a new line.
+* @s: string to print.
+* Return: nothing
+**/
+void _puts_recursion(char *s)
+{
+	int len = 0;
+
+	while (s[len] != 0)
+	{
+		_putchar(s[len] + '0');
+		len++;
+	}
+}
