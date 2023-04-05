408e215bd81822893574caba7ff896ca78590235
Yassine Rakibi <yassine@arcraven.com>
2023-04-05 17:58:51 +0200

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+# include "main.h"
+/**
+* _sqrt_recursion - function that returns the natural
+* square root of a number.
+* @n: int.
+* Return: -1 if error, natural square root.
+**/
+
+int _sqrt_recursion(int n)
+{
+	return (sqr(n, 1));
+}
+
+/**
+* sqr - square root function
+* @s: number to calculate square root for
+* @w: number
+* Return: int natural square root
+**/
+int sqr(int s, int w)
+{
+	if (w < 0)
+	{
+		return (-1);
+	}
+	if (w * w > s)
+	{
+		return (-1);
+	}
+	if (w * w == s)
+	{
+		return (sqr(s, w++));
+	}
+}
+
