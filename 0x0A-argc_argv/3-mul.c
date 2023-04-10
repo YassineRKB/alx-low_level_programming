c6a309e66b1482322c43df79d97041048b561c34
Yassine Rakibi <yassine@arcraven.com>
2023-04-10 11:17:22 +0200

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+* main - entry point to code
+* @argc: number of arguments
+* @argv: value of arguments
+* Return: 1 or 0.
+**/
+int main(int argc, char **argv)
+{
+	int a = argc - 1;
+
+	if (a <= 0)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	int r, i;
+
+	r = 1;
+	for (i = 1; i <= a; i++)
+	{
+		r *= argv[i];
+	}
+	printf("%d\n", r);
+	return (0);
+}
