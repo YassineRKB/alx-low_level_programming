17b72a0040138c66956d0d42de8474ab909485e1
Yassine Rakibi <yassine@arcraven.com>
2023-04-10 10:30:25 +0200

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+/**
+* main - entry point to code
+* @argc: number of arguments
+* @argv: value of arguments
+* Return: nothing void.
+**/
+void main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+}
