a4b98dd71ec2d9427aa2b9117f04f966adebbebd
Yassine Rakibi <yassine@arcraven.com>
2023-04-10 11:25:15 +0200

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
 * main - entry point to code
 * @argc: number of arguments
@@ -14,13 +15,64 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		return (1);
 	}
-	int r, i;
+	int r, i, n;
 
 	r = 1;
 	for (i = 1; i <= a; i++)
 	{
-		r *= (int) argv[i];
+		n =  atoi(argv[i]);
+		r *= n;
 	}
 	printf("%d\n", r);
 	return (0);
 }
+
+/**
+ * _atoi - function that converts string to int
+ * @s: string to be converted
+ * Return: converted number or 0
+*/
+int _atoi(char *s)
+{
+	int i = 0;
+	int n = 0;
+	int len = 0;
+	int j = 0;
+	int c = 0;
+	int d;
+
+	d = 0;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	while (i < len && c == 0)
+	{
+		if (s[i] == '-')
+		{
+			++j;
+		}
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			d = s[i] - '0';
+			if (j % 2)
+			{
+				d = -d;
+			}
+			n = n * 10 + d;
+			c = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+			{
+				break;
+			}
+			c = 0;
+		}
+		i++;
+	}
+	if (c == 0)
+	{
+		return (0);
+	}
+	return (n);
+}
+
