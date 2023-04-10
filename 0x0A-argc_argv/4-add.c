2f45b449e568b4a1f0138474d479b4254d770531
Yassine Rakibi <yassine@arcraven.com>
2023-04-10 13:38:14 +0200

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include "main.h"
+#include <ctype.h>
+#include <stdlib.h>
 /**
 * main - entry point to code
 * @argc: number of arguments
@@ -11,7 +13,9 @@ int main(int argc, char **argv)
 	int a = argc - 1;
 	int r;
 	int i;
+	int j;
 	int n;
+	char c;
 
 	r = 0;
 	if (a == 0)
@@ -22,81 +26,23 @@ int main(int argc, char **argv)
 
 	for (i = 1; i <= a; i++)
 	{
-		n =  _atoi(argv[i]);
-		if (_isdigit(n))
+
+		c = *argv[i];
+		(void) c;
+		j = 0;
+		while (argv[i][j] != '\0')
 		{
-			printf("Error\n");
-			return (0);
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (0);
+			}
+			j++;
 		}
+
+		n = atoi(argv[i]);
 		r += n;
 	}
 	printf("%d\n", r);
 	return (0);
-}
-
-/**
- * _atoi - function that converts string to int
- * @s: string to be converted
- * Return: converted number or 0
-*/
-int _atoi(char *s)
-{
-	int i = 0;
-	int n = 0;
-	int len = 0;
-	int j = 0;
-	int c = 0;
-	int d;
-
-	d = 0;
-	while (s[len] != '\0')
-	{
-		len++;
-	}
-	while (i < len && c == 0)
-	{
-		if (s[i] == '-')
-		{
-			++j;
-		}
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			d = s[i] - '0';
-			if (j % 2)
-			{
-				d = -d;
-			}
-			n = n * 10 + d;
-			c = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-			{
-				break;
-			}
-			c = 0;
-		}
-		i++;
-	}
-	if (c == 0)
-	{
-		return (0);
-	}
-	return (n);
-}
-
-/**
-* _isdigit - function that checks @c is digit
-* @c: evaluation target
-* Return: 1 or 0
-*/
-
-int _isdigit(int c)
-{
-	if (c >= 48 && c <= 57)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
 }
