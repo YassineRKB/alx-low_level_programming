897cc7e1af0676a6ce032bd40af6e8de9e1ad8fc
Yassine Rakibi <yassine@arcraven.com>
2023-04-10 14:09:48 +0200

    task 100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,16 +3,38 @@
 * main - entry to code
 * @argc: number of arguments
 * @argv: value of arguments
-* Return:
+* Return: 1 or 0
 **/
 
 int main(int argc, char **argv)
 {
 	int a = argc - 1;
+	int i, j, r;
+	int c[] = {25, 10, 5, 2, 1}
 
+	r = 0;
+	j = 0;
 	if (a < 1 || a > 1)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	j = atoi(argv[1])
+
+	if (j < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 0; j >= 0 && i <= 4; i++)
+	{
+		while (j >= c[i])
+		{
+			j -= c[i];
+			r++;
+		}
+	}
+	printf("%d\n", r);
+	return (0);
 }
