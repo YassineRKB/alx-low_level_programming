a19db488ad8ebbef0f0be22a64c3cf452ca5bf73
Yassine Rakibi <yassine@arcraven.com>
2023-04-11 18:18:02 +0200

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	size = (sizeof(int) * height);
+	size = (sizeof(int *) * height);
 	p = (int **) malloc(size);
 
 	if (p == NULL)
