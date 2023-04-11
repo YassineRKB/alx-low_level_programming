711ee75a309312ff4aba34b1c5d7793e34b1668c
Yassine Rakibi <yassine@arcraven.com>
2023-04-11 19:47:31 +0200

    task 101 test

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,31 @@
+# include <stdlib.h>
+# include "main.h"
+
+/**
+* strtow - function that splits a string into words.
+* @str: string
+* Return: pointer or NULL if str == NULL or str == ""
+**/
+
+char **strtow(char *str)
+{
+	char **p;
+	int len = 0, i;
+
+	if (str == NULL || str == "")
+		return (NULL);
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	p = (char **) malloc(sizeof(char *) * (len + 1))
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < len; i++)
+	{
+		if (str[i] != ' ')
+		{
+			p[i] = str[i] + '\0';
+		}
+	}
+}
