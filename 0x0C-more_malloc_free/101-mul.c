08a31fd0292372a26544156dc30a17475212a567
Yassine Rakibi <yassine@arcraven.com>
2023-04-13 18:18:09 +0200

    task 101

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,22 @@
+# include <stdio.h>
+
+/**
+* main - a program that multiplies two positive numbers.
+* @argc: arguments count.
+* @argv: arguments value.
+* Return: int result, 1 if error.
+**/
+int main(int argc, int **argv)
+{
+	unsigned int r, n1, n2;
+
+	if (argc != 3)
+		printf("Error");
+		return (1);
+	if (!isdigit(argv[1]) || !isdigit(argv[2]))
+		printf("Error");
+	n1 = argv[1];
+	n2 = argv[2];
+	r = n1 * n2;
+	return (r);
+}
