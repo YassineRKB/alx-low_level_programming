3e88aa9b7d47749456d65e38a2a5ab8bdc0858ce
Yassine Rakibi <yassine@arcraven.com>
2023-04-13 21:58:55 +0200

    task 101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -10,22 +10,17 @@
 **/
 int main(int argc, char **argv)
 {
-	unsigned long int r, n1, n2;
-	char *s1, *s2;
+	int r, n1, n2;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (98);
 	}
-	n1 = strtol(argv[1], &s1, 10);
-	n2 = strtol(argv[2], &s2, 10);
-	if (strcmp(s1, "") != 0 ||  strcmp(s2, "") != 0)
-	{
-		printf("Error\n");
-		return (98);
-	}
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
+
 	r = n1 * n2;
-	printf("%ld\n", r);
+	printf("%d\n", r);
 	return (r);
 }
