377cd894a9184c650f52d80946267ee1e30c3375
Yassine Rakibi <yassine@arcraven.com>
2023-04-13 16:53:56 +0200

    task 100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,28 +11,21 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *p;
+	unsigned int *p;
 	unsigned int i;
 
 	if (ptr == NULL)
-		ptr = malloc(newsize);
+		ptr = malloc(new_size);
+
 	if (new_size == old_size)
 		return (ptr);
+
 	if (new_size == 0 && ptr != NULL)
+	{
 		free(ptr);
 		return (NULL);
-	p = malloc(sizeof(*p) * (newsize - oldsize + 1));
-	if (p == NULL)
-		return (NULL);
-	if (newsize > oldsize)
-	{
-		for (i = 0; oldsize < newsize; i++)
-		{
-			if (i <= oldsize)
-				p[i] = ptr[i];
-			if (i > oldsize && i <= newsize)
-				p[i] = 0;
-		}
 	}
+
+	p = malloc(new_size);
 	return (p);
 }
