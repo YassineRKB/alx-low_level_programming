a580dc9483b24ce8236a8ee4535c923adc66f8a0
Yassine Rakibi <yassine@arcraven.com>
2023-04-17 18:35:01 +0200

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,5 @@
 # include <stdio.h>
+# include <stdlib.h>
 # include <string.h>
 # include "dog.h"
 
@@ -16,7 +17,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (name == NULL || age <= 0 || owner == NULL)
 		return (NULL);
-	dog = malloc(sizeof(dog_t *))
+	dog = malloc(sizeof(dog_t *));
 	if (!dog)
 	{
 		free(dog);
