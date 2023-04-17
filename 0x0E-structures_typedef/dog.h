519386918195d40d51a03d4ffc1e55c96defa55f
Yassine Rakibi <yassine@arcraven.com>
2023-04-17 17:18:51 +0200

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef DOG_H
-#define DOG_h
+#define DOG_H
+/**
+* struct dog - new type struct dog
+* @name: the dog's name
+* @owner: the dog's owner
+* @age: the dog's age
+**/
 struct dog
 {
 	char *name;
