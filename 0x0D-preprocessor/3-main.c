f0772fd59be46a08a14f0766906b31cf9baf5fa5
Yassine Rakibi <yassine@arcraven.com>
2023-04-17 16:28:47 +0200

    task 3

--- 0x0D-preprocessor/3-main.c
+++ 0x0D-preprocessor/3-main.c
@@ -10,10 +10,13 @@
 int main(void)
 {
     int i;
-    int j;
+    int j,l,m,n;
 
     i = ABS(-98) * 10;
     j = ABS(98) * 10;
-    printf("%d, %d\n", i, j);
+	l =ABS(0);
+	m =ABS(-2);
+	n = ABS(1);
+    printf("i:%d, j:%d, l:%d, m:%d, n:%d", i, j,l,m,n);
     return (0);
 }
