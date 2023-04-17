a7baaf9f4f3971b9900fb8e072c434e6a546f458
Yassine Rakibi <yassine@arcraven.com>
2023-04-17 18:01:19 +0200

    task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,9 +9,10 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		!(d->name) ? printf("Name: (nil)\n") : printf("Name: %s\n", d->name);
-		!(d->age) ? printf("Age: (nil)\n") : printf("Age: %f\n", d->age);
-		!(d->owner) ? printf("Owner: (nil)\n") : printf("Owner: %s\n", d->owner);
+		d->name == NULL ? printf("Name: (nil)\n") : printf("Name: %s\n", d->name);
+		d->age == NULL ? printf("Age: (nil)\n") : printf("Age: %f\n", d->age);
+		printf("Owner: ");
+		d->owner == NULL ? printf("(nil)\n") : printf("%s\n", d->owner);
 	}
 
 }
