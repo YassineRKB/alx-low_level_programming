c9382319ac952f4ca9e2ca8a01dcca46d80322f3
Yassine Rakibi <yassine@arcraven.com>
2023-04-19 19:55:56 +0200

    task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+# include "function_pointers.h"
+# include <stdio.h>
+
+/**
+* array_iterator - function that invokes a function passed as parameter
+* on each element of array.
+* @array: array containning int elements.
+* @size: size of array.
+* @action: function to be invoked.
+* Return: void.
+**/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array && size && action)
+	{
+		while (*array)
+		{
+			action(*array);
+			array++;
+		}
+	}
+}
