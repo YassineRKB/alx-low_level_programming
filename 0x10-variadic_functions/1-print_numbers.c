41b0744cc5da1a4e6da3123bfc194d5d92fc42b3
Yassine Rakibi <yassine@arcraven.com>
2023-04-20 16:13:14 +0200

    task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,10 +18,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(nums, n);
 	for (i = 0; i < n; i++)
 	{
-		if (separator == NULL)
-			printf("%d ", va_arg(nums, int));
+
+		printf("%d", va_arg(nums, int));
 		if (separator != NULL && i != n - 1)
-			printf("%d%s ", separator);
+			printf("%s", separator);
 
 	}
 	printf("\n");
