3b90b650ee706c1680e595ecfa770bc3137c8638
Yassine Rakibi <yassine@arcraven.com>
2023-04-27 20:51:54 +0200

    task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+* free_list - function to free linked lists
+* @head: node to be freed
+* Return: void
+**/
+void free_list(list_t *head)
+{
+	while (head != NULL)
+	{
+		head = head->next;
+		free(head);
+	}
+}
