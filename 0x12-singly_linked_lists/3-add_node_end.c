fdb81e47cf7c6dcdd7a14e945532ec0b50dab710
Yassine Rakibi <yassine@arcraven.com>
2023-04-27 20:31:43 +0200

    task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,7 +10,7 @@
 **/
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node;
+	list_t *new_node, *temp_node;
 	size_t len = 0;
 
 	if (str != NULL)
@@ -24,15 +24,15 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->str = strdup(str);
 	new_node->len = len;
 	new_node->next = NULL;
-
-	if (*head != NULL)
+	temp_node = *head;
+	if (temp_node != NULL)
 	{
-		while (*head->next != NULL)
-			*head = *head->next;
-		*head->next = new_node;
+		while (temp_node->next != NULL)
+			temp_node = temp_node->next;
+		temp_node->next = new_node;
 	}
 	else
-		*head->next = new_node;
+		*head = new_node;
 
-	return (0);
+	return (*head);
 }
