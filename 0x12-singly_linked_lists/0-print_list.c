d9e3a6e3f6922587b73159144f18a9b2906e5fa7
Yassine Rakibi <yassine@arcraven.com>
2023-04-27 19:08:33 +0200

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,16 +10,16 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t n = 1;
+	size_t n = 0;
 
 	if (h == NULL)
 		return (0);
 	while (h->next != NULL)
 	{
-		if (h->str != NULL)
-			printf("[%d] %s\n", h->len, h->str);
-		else
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		n++;
 	}
