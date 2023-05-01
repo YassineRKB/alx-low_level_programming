1556a2f258d380ff7e72b3a34990dfdd8c3a29e7
Yassine Rakibi <yassine@arcraven.com>
2023-05-01 21:50:19 +0200

    task 12 last check, come to my van i have candy

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,31 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
+size_t loop_list(const listint_t *head);
 
 /**
 * print_listint_safe - function that prints a linked list
 * @head: head node of the linked list
 * Return: num of nodes in linked list, 98 on fail
 ***/
-
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t n = 0;
+	size_t n = 0, idx = 0;
 
-	while (head != NULL)
+	if (head == NULL)
+		exit(98);
+	n = loop_list(head);
+	if (n != 0)
 	{
-		printf("[%p] %d\n", (void *) head, head->n);
-		if (head <= head->next)
+		while (idx < n)
 		{
-			head = head->next;
-			printf("-> [%p] %d\n", (void *)head, head->n);
-			n++;
-			break;
+			printf("[%p] %d\n", (void *) head, head->n);
+			head = head->next, idx++;
 		}
-		if (head == NULL)
-			exit(98);
-		head = head->next;
-		n++;
+		printf("-> [%p] %d\n", (void *) head, head->n);
+	}
+	else
+	{
+		while (head != NULL)
+		{
+			printf("[%p] %d\n", (void *) head, head->n);
+			head = head->next, n++;
+		}
 	}
 	return (n);
+}
+/**
+* loop_list - function to implement the floyed algorithm
+* @head: linked list to be searched.
+* Return: number of nodes/
+**/
+size_t loop_list(const listint_t *head)
+{
+	const listint_t *fast = head, *slow = head;
+	size_t n;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+	fast = fast->next->next;
+	slow = slow->next;
+	n = 1;
+	while (fast)
+	{
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				n++;
+				slow = slow->next;
+				fast = fast->next;
+			}
+			slow = slow->next;
+			while (slow != fast)
+			{
+				n++;
+				slow = slow->next;
+			}
+			return (n);
+		}
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	return (0);
 }
