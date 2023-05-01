b89b5e525e68de1ad61c3af85a05ab9292b8638f
Yassine Rakibi <yassine@arcraven.com>
2023-05-01 20:34:24 +0200

    task 12 last check, come to my van i have candy

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -17,6 +17,7 @@ size_t print_listint_safe(const listint_t *head)
 		printf("[%p] %d\n", (void *) head, head->n);
 		if (head <= head->next)
 		{
+			head = head->next;
 			printf("-> [%p] %d\n", (void *)head, head->n);
 			n++;
 			break;
