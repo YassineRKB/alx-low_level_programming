b3d1dc269dd9bfc9bb403fdaab72b8831a2b46ce
Yassine Rakibi <yassine@arcraven.com>
2023-05-01 05:24:10 +0200

    task

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+* free_listint - function to free linked lists
+* @head: head node of linked list
+* Return: void.
+**/
+void free_listint(listint_t *head)
+{
+	listint_t *node;
+
+	while ((node = head))
+	{
+		head = head->next;
+		free(node);
+	}
+
+}
