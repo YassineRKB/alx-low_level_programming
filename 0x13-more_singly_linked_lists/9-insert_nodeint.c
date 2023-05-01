1de88d7304c91dcbd12a7dc9b41a39169b9f30e9
Yassine Rakibi <yassine@arcraven.com>
2023-05-01 16:38:29 +0200

    task 9: for this task to work some white hair strand were sacrificed.

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+* insert_nodeint_at_index - function to insert a new node in given position.
+* @head: head node of the linked list.
+* @idx: position where the new node will be inserted.
+* @n: data n of new node.
+* Return: linked list.
+**/
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *node, *h;
+	unsigned int i = 0;
+
+	h = *head;
+	node = malloc(sizeof(listint_t));
+	if (node == NULL)
+		return (NULL);
+	node->n = n;
+	if (idx != 0)
+	{
+		while (h != NULL && i != idx - 1)
+			h = h->next, i++;
+		node->next = h->next;
+		h->next = node;
+		while (node != NULL)
+			node = node->next;
+	}
+	else
+	{
+		node->next = *head;
+		*head = node;
+	}
+	return (node);
+}
