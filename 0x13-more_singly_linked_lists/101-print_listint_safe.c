d904134982e3d59081a1ef1456d08b48386f24b2
Yassine Rakibi <yassine@arcraven.com>
2023-05-01 19:59:21 +0200

    task 12 accuresd infinity loop begone

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,28 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
+
 /**
 * print_listint_safe - function that prints a linked list
 * @head: head node of the linked list
 * Return: num of nodes in linked list, 98 on fail
 ***/
+
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t n;
+	size_t n = 0;
 
-	for (n = 0; h != NULL; n++)
+	while (head != NULL)
 	{
-		head = head->next;
+		if (head == NULL)
+			exit(98);
 		printf("[%p] %d\n", (void *) head, head->n);
-		if (head = head->next)
+		if (head < head->next)
 		{
 			head = head->next;
-			printf("-> [%p] %d\n", (void *)h, h->n);
+			printf("-> [%p] %d\n", (void *)head, head->n);
 			n++;
 			break;
 		}
-		if (head == NULL)
-			exit(98);
+		head = head->next;
+		n++;
 	}
 	return (n);
 }
