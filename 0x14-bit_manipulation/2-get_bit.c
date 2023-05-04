de30986f5bb556538c902f1340d62ab0372e8a46
Yassine Rakibi <yassine@arcraven.com>
2023-05-04 18:56:07 +0200

    task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,5 +9,7 @@
 **/
 int get_bit(unsigned long int n, unsigned int index)
 {
-	(index >= (sizeof(n) * 8)) ? return (-1) : return ((n >>= index) & 1);
+	if (index >= (sizeof(n) * 8))
+		return (-1);
+	return ((n >>= index) & 1);
 }
