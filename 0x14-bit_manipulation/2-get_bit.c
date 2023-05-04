fb995e3176b68b74effee920f7b3e48c8207db6a
Yassine Rakibi <yassine@arcraven.com>
2023-05-04 18:52:48 +0200

    task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+* get_bit - function to return the value of the bit in given index
+* @n: a number.
+* @index: target index starting from 0.
+* Return: index bit value on success, or -1 on fail
+**/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index < (sizeof(n) * 8))
+		return (-1);
+	return ((n >>= index) & 1);
+}
