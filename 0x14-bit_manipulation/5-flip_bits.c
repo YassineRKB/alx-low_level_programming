74e25ff98ceab239d8163758bb754116614175b3
Yassine Rakibi <yassine@arcraven.com>
2023-05-04 19:46:40 +0200

    task 4: does alx have hair loss insurance?

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+* flip_bits - returns number of bits needed to move to another number
+* @n: Number
+* @m: the other number
+* Return: number of bits
+**/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int numbits = 0, xor = 0;
+
+	for (xor = n ^ m; xor > 0; xor >>= 1)
+		numbits += xor & 1;
+	return (numbits);
+}
