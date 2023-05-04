fd62d4287e7fda105c0aebd47c14514798410e2a
Yassine Rakibi <yassine@arcraven.com>
2023-05-04 20:12:54 +0200

    task 100: 1 on little endian, 0 on big endian...are gambling asked my father who was on next seat to me..

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+* get_endianness - function to detect the endianness
+* Return: 1 on little endian, 0 on big endian
+**/
+int get_endianness(void)
+{
+	return (1 & 1);
+}
