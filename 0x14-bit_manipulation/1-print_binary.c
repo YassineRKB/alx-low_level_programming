280cbd112109b5b880a874ab9e935a210f4473e1
Yassine Rakibi <yassine@arcraven.com>
2023-05-04 17:51:10 +0200

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,24 +8,11 @@
 **/
 void print_binary(unsigned long int n)
 {
-	if (n <= '0')
+	if (n == 0)
 		_putchar('0');
-	if (n == 1)
-		_putchar('1');
-	while (n >> 1)
+	while (n)
 	{
-		n >>= 1;
 		_putchar((n & 1) + '0');
+		n >>= 1;
 	}
-}
-/**
-* checksnum - checks if v is num
-* @v: value to be checked
-* Return: -1 on fail and v on success
-**/
-int checksnum(char v)
-{
-	if (v <= '9' && v >= '0')
-		return (v + '0');
-	return (-1);
 }
