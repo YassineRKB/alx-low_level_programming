f61cfc4b9069645e45e4ad608d6d5ef4002ba64b
Yassine Rakibi <yassine@arcraven.com>
2023-06-15 20:38:51 +0200

    2-add_dnodeint.c betty compliance

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,11 +10,17 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *h = *head, *new;
 
 	new = malloc(sizeof(dlistint_t));
-	if (!new) return (NULL);
+	if (!new)
+		return (NULL);
 	new->n = n;
 	new->prev = NULL;
-	if (h) {new->next = h; h->prev = new;}
-	else {new->next = NULL;}
+	if (h)
+	{
+		new->next = h;
+		h->prev = new;
+	}
+	else
+		new->next = NULL;
 	*head = new;
 	return (new);
 }
