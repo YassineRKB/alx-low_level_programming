3d072292f237f72945f50fdaccf897297bba119a
Yassine Rakibi <yassine@arcraven.com>
2023-06-20 00:19:53 +0200

    101-make_me_win.sh changing wget flags from output to directory prefix

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -o /tmp/malware.so -i https://github.com/YassineRKB/alx-low_level_programming/blob/main/0x18-dynamic_libraries/malware.so
+wget --directory-prefix=/tmp/ https://github.com/YassineRKB/alx-low_level_programming/blob/main/0x18-dynamic_libraries/malware.so
 export LD_PRELOAD=/tmp/malware.so
