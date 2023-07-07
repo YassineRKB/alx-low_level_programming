bd452be5623fa741087168dd6c43f7de5253c374
Yassine Rakibi <yassine@arcraven.com>
2023-07-07 23:07:56 +0200

    2-key_index.c validate

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,10 +1,14 @@
 #include "hash_tables.h"
 /**
- * key_index - function to get index of a key 
+ * key_index - function to get index of a key
+ * @key: key
+ * @size: size
+ * Return: index
 */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int res = hash_djb2(key);
+
 	res = res % size;
 	return (res);
 }
