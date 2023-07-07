8c7650daec2301ed49ed4caad768e46941b3e6d9
Yassine Rakibi <yassine@arcraven.com>
2023-07-07 22:29:45 +0200

    0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - func to create a hash table
+ * @size: size of the size of the array
+ * Return: pointer to created hash table
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *nHolyHashTable;
+
+	if (!size)
+		return (NULL);
+	nHolyHashTable = malloc(sizeof(hash_table_t));
+	if (!nHolyHashTable)
+		return (NULL);
+	nHolyHashTable->size = size;
+	nHolyHashTable->array = malloc(
+		sizeof(hash_node_t *) * size
+	);
+	if (!nHolyHashTable->array)
+		return (NULL);
+	else
+		return (nHolyHashTable);
+}
