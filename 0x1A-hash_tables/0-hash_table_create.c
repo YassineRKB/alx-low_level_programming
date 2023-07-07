38a597ec3923054177ba4fbe492d614f57238c84
Yassine Rakibi <yassine@arcraven.com>
2023-07-07 22:34:03 +0200

    0-hash_table_create.c checker

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,8 +8,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *nHolyHashTable;
 
-	if (!size)
-		return (NULL);
 	nHolyHashTable = malloc(sizeof(hash_table_t));
 	if (!nHolyHashTable)
 		return (NULL);
