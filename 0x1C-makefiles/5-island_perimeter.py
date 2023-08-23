f8c72e773e513f68ba64edfcb269ea475998540d
Yassine Rakibi <yassine@arcraven.com>
2023-08-24 00:21:37 +0200

    5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Island Perimeter Module"""
+
+
+def island_perimeter(grid):
+    """Funtionc to calculate the perimeter of grid where "1" is found"""
+    perimeter = 0
+    grlen = len(grid)
+    grlenRow = len(grid[0])
+    for row in range(grlen):
+        for col in range(grlenRow):
+            if grid[row][col] == 1:
+                if col == (grlenRow - 1) or grid[row][col + 1] == 0:
+                    perimeter += 1
+                if row == 0 or grid[row - 1][col] == 0:
+                    perimeter += 1
+                if row == (grlen - 1) or grid[row + 1][col] == 0:
+                    perimeter += 1
+                if col == 0 or grid[row][col - 1] == 0:
+                    perimeter += 1 
+    return perimeter
